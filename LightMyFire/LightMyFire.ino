7faa18a680c7037a03a2ca5cb637d6cf0b74b635
Axel Roest <axel@phlux.us>
2019-10-28 17:02:14 +0100

    Perfect, the firebase to neopixel demo works

--- LightMyFire/LightMyFire.ino
+++ LightMyFire/LightMyFire.ino
@@ -16,19 +16,16 @@
 //#include "fonts.h"
 //#include "Drawdemo.h"
 
-/*
+#include "Colorconversions.h"
+#include "NeoPixel.h"
 
-*/
 FirebaseData firebaseData;
 
 /********************************
  * Function headers
  ********************************/
 void wifiSetup();
-void fbSetup();
-void firebaseSetTest(String path);
-void firebaseGetTest(String path);
-void printJsonObjectContent(FirebaseData &data);
+void setNeopixel(float hue);
 
 /********************************
  * Functions
@@ -45,14 +42,15 @@ void setup() {
 }
 
 void loop() {
-//  if (Firebase.getFloat(firebaseData, "/esp32/academy/hue")) {
-//
-//    if (firebaseData.dataType() == "float") {
-//      Serial.println(firebaseData.floatData());
-//    }
-//  } else {
-//    Serial.println(firebaseData.errorReason());
-//  }
+  if (Firebase.getDouble(firebaseData, "/lights/academy/hue")) {
+    float hue = firebaseData.floatData();
+     Serial.print (hue);
+     setNeopixels((float) hue);
+//      Serial.print("Returned datatype: ");
+//      Serial.println(firebaseData.dataType());
+  } else {
+    Serial.println(firebaseData.errorReason());
+  }
 }
 
 void wifiSetup() {
@@ -71,134 +69,10 @@ void wifiSetup() {
   Serial.println();
 }
 
-void fbSetup() {
+void setNeopixels(float hue) {
+  int rgb[3];
 
-  //3. Declare the Firebase Data object in global scope
-
-  //4. Setup Firebase credential in setup()
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-
-  //5. Optional, set AP reconnection in setup()
-  Firebase.reconnectWiFi(true);
-
-  //6. Optional, set number of auto retry when read/store data
-  Firebase.setMaxRetry(firebaseData, 3);
-
-  //Set database read timeout to 1 minute (max 15 minutes)
-  Firebase.setReadTimeout(firebaseData, 1000 * 60);
-
-  //7. Optional, set number of read/store data error can be added to queue collection
-  Firebase.setMaxErrorQueue(firebaseData, 30);
-
-  //8. Optional, use classic HTTP GET and POST requests.
-  //This option allows get and delete functions (PUT and DELETE HTTP requests) works for
-  //device connected behind the Firewall that allows only GET and POST requests.
-  // Firebase.enableClassicRequest(firebaseData, true);
-
-  //tiny, small, medium, large and unlimited.
-  //Size and its write timeout e.g. tiny (1s), small (10s), medium (30s) and large (60s).
-  Firebase.setwriteSizeLimit(firebaseData, "tiny");
-
-}
-
-//  FirebaseJson json;
-
-void firebaseSetTest(String path) {
-  Serial.println("------------------------------------");
-  Serial.println("Set double test...");
-
-  for (uint8_t i = 0; i < 10; i++)
-  {
-
-    if (Firebase.setDouble(firebaseData, path + "/Double/Data" + (i + 1), ((i + 1) * 10) + 0.123456789))
-    {
-      Serial.println("PASSED");
-      Serial.println("PATH: " + firebaseData.dataPath());
-      Serial.println("TYPE: " + firebaseData.dataType());
-      Serial.println("ETag: " + firebaseData.ETag());
-      Serial.print("VALUE: ");
-      if (firebaseData.dataType() == "int")
-        Serial.println(firebaseData.intData());
-      else if (firebaseData.dataType() == "float")
-        Serial.println(firebaseData.floatData(), 5);
-      else if (firebaseData.dataType() == "double")
-        printf("%.9lf\n", firebaseData.doubleData());
-      else if (firebaseData.dataType() == "boolean")
-        Serial.println(firebaseData.boolData() == 1 ? "true" : "false");
-      else if (firebaseData.dataType() == "string")
-        Serial.println(firebaseData.stringData());
-      else if (firebaseData.dataType() == "json")
-        printJsonObjectContent(firebaseData);
-      Serial.println("------------------------------------");
-      Serial.println();
-    }
-    else
-    {
-      Serial.println("FAILED");
-      Serial.println("REASON: " + firebaseData.errorReason());
-      Serial.println("------------------------------------");
-      Serial.println();
-    }
-  }
-}
-
-void firebaseGetTest(String path) {
-  Serial.println("------------------------------------");
-  Serial.println("Get double test...");
-
-  for (uint8_t i = 0; i < 10; i++)
-  {
-
-    if (Firebase.getInt(firebaseData, path + "/Double/Data" + (i + 1)))
-    {
-      Serial.println("PASSED");
-      Serial.println("PATH: " + firebaseData.dataPath());
-      Serial.println("TYPE: " + firebaseData.dataType());
-      Serial.println("ETag: " + firebaseData.ETag());
-      Serial.print("VALUE: ");
-      if (firebaseData.dataType() == "int")
-        Serial.println(firebaseData.intData());
-      else if (firebaseData.dataType() == "float")
-        Serial.println(firebaseData.floatData(), 5);
-      else if (firebaseData.dataType() == "double")
-        printf("%.9lf\n", firebaseData.doubleData());
-      else if (firebaseData.dataType() == "boolean")
-        Serial.println(firebaseData.boolData() == 1 ? "true" : "false");
-      else if (firebaseData.dataType() == "string")
-        Serial.println(firebaseData.stringData());
-      else if (firebaseData.dataType() == "json")
-        printJsonObjectContent(firebaseData);
-      Serial.println("------------------------------------");
-      Serial.println();
-    }
-    else
-    {
-      Serial.println("FAILED");
-      Serial.println("REASON: " + firebaseData.errorReason());
-      Serial.println("------------------------------------");
-      Serial.println();
-    }
-  }
-}
-
-void printJsonObjectContent(FirebaseData &data) {
-  size_t tokenCount = data.jsonObject().parse(false).getJsonObjectIteratorCount();
-  String key;
-  String value;
-  FirebaseJsonObject jsonParseResult;
-  Serial.println();
-  for (size_t i = 0; i < tokenCount; i++)
-  {
-    data.jsonObject().jsonObjectiterator(i, key, value);
-    jsonParseResult = data.jsonObject().parseResult();
-    Serial.print("KEY: ");
-    Serial.print(key);
-    Serial.print(", ");
-    Serial.print("VALUE: ");
-    Serial.print(value);
-    Serial.print(", ");
-    Serial.print("TYPE: ");
-    Serial.println(jsonParseResult.type);
-
-  }
+  hsi2rgb(hue * 360.0, 1.0, 0.5, rgb);
+  Serial.printf("RGB: %u, %u, %u\n", rgb[0], rgb[1], rgb[2]);
+  LedRGBON(hue);
 }
---
+++ LightMyFire/NeoPixel.h
@@ -0,0 +1,101 @@
+/* ======================================================================
+  Function: NeoPixel
+  ====================================================================== */
+// RGB Led on GPIO0 comment this line if you have no RGB LED
+#define RGB_LED_PIN 12
+
+// Number of RGB Led (can be a ring or whatever) 
+#define RGB_LED_COUNT 2
+
+// Select The line of your LED Type (see NeoPixel Library)
+//#define RGB_TYPE NeoGrbwFeature
+//#define RGB_TYPE NeoRgbwFeature
+#define RGB_TYPE NeoGrbFeature
+// #define RGB_TYPE NeoRgbFeature // hsl wrong for my neopixels, Axel
+
+#ifdef RGB_LED_PIN
+#include <NeoPixelBus.h>
+#endif
+
+#ifdef RGB_LED_PIN
+  #ifdef ARDUINO_ARCH_ESP8266
+    NeoPixelBus<RGB_TYPE, NeoEsp8266BitBang800KbpsMethod>rgb_led(RGB_LED_COUNT, RGB_LED_PIN);
+  #else
+    NeoPixelBus<RGB_TYPE, NeoEsp32BitBang800KbpsMethod>rgb_led(RGB_LED_COUNT, RGB_LED_PIN);
+  #endif
+#endif
+
+
+#ifdef RGB_LED_PIN
+void LedRGBOFF(uint16_t led = 0);
+void LedRGBON (float hue, uint16_t led = 0);
+#else
+void LedRGBOFF(uint16_t led = 0) {};
+void LedRGBON (float hue, uint16_t led = 0) {};
+#endif
+
+uint8_t rgb_luminosity    = 50 ;    // Luminosity from 0 to 100%
+
+
+#ifdef RGB_LED_PIN
+/* ======================================================================
+  Function: LedRGBON
+  Purpose : Set RGB LED strip color, but does not lit it
+  Input   : Hue of LED (0..360)
+          led number (from 1 to ...), if 0 then all leds
+  Output  : -
+  Comments:
+  ====================================================================== */
+void LedRGBON (float hue, uint16_t led)
+{
+  uint8_t start = 0;
+  uint8_t end   = RGB_LED_COUNT - 1; // Start at 0
+
+  // Convert to neoPixel API values
+  // H should be between 0.0 and 1.0
+  // S is saturation keep it to 1
+  // L is brightness should be between 0.0 and 0.5
+  // rgb_luminosity is between 0 and 100 (percent)
+  RgbColor target = HslColor( hue, 1.0f, 0.005f * rgb_luminosity);
+
+  // just one LED ?
+  // Strip start 0 not 1
+  if (led) {
+    led--;
+    start = led ;
+    end   = start ;
+  }
+
+  for (uint8_t i = start ; i <= end; i++) {
+    rgb_led.SetPixelColor(i, target);
+    rgb_led.Show();
+  }
+}
+
+/* ======================================================================
+  Function: LedRGBOFF
+  Purpose : light off the RGB LED strip
+  Input   : Led number starting at 1, if 0=>all leds
+  Output  : -
+  Comments: -
+  ====================================================================== */
+void LedRGBOFF(uint16_t led)
+{
+  uint8_t start = 0;
+  uint8_t end   = RGB_LED_COUNT - 1; // Start at 0
+
+  // just one LED ?
+  if (led) {
+    led--;
+    start = led ;
+    end   = start ;
+  }
+
+  // stop animation, reset params
+  for (uint8_t i = start ; i <= end; i++) {
+    // clear the led strip
+    rgb_led.SetPixelColor(i, RgbColor(0));
+    rgb_led.Show();
+  }
+}
+#endif
