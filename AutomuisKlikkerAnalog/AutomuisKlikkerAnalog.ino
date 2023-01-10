e2dac2e60bdb58c932af1d85b1dec84711126aa0
Axel Roest <axel@phlux.us>
2023-01-10 21:11:54 +0100

    Add Blynk version of mouseclicker

--- AutomuisKlikkerAnalog/AutomuisKlikkerAnalog.ino
+++ AutomuisKlikkerAnalog/AutomuisKlikkerAnalog.ino
@@ -4,45 +4,21 @@
  pin, without using the delay() function.  This means that other code
  can run at the same time without being interrupted by the LED code.
 
- The circuit:
- * LED attached from pin 13 to ground.
- * Note: on most Arduinos, there is already an LED on the board
- that's attached to pin 13, so no hardware is needed for this example.
- 
- created 2005
- by David A. Mellis
- modified 8 Feb 2010
- by Paul Stoffregen
- modified 11 Nov 2013
- by Scott Fitz                                                                                                                                                                                                                                                                                                                             gerald
- 
- 
- This example code is in the public domain.
- 
- http://www.arduino.cc/en/Tutorial/BlinkWithoutDelay
+  Version meant for the digispark
+  Version meant for the nano
 
  Axel Roest
  This version uses an analog value, derived from a potmeter, to set the click speed
  
  */
-
-typedef enum buttonStateType {
-  none = -1,
-  low = 0,
-  high = 1
-};
-
+ 
 // constants
-#define BUTTONCOUNT 2
-#define DEBUG 1
 
+// #define DEBUG 1
 
-const int upPin =  3;      // the number of the LED pin
-const int downPin =  4;      // the number of the LED pin
-const int upLedPin = 8;
-const int downLedPin = 9;
-const int ledPin =  13;      // the number of the LED pin
-const int analogPin = 0;
+const int ledPin =  3;      // the number of the LED pin
+const int switchPin =  2;      // the number of the Switch pin
+const int analogPin = A7;
 
 // Variables will change :
 int ledState = LOW;             // ledState used to set the LED
@@ -50,22 +26,16 @@ long interval = 200;           // interval at which to blink (milliseconds)
 
 // Generally, you should use "unsigned long" for variables that hold time
 // The value will quickly become too large for an int to store
-const unsigned long int clickDuration = 200;
+const unsigned long int clickDuration = 40;
 
 void setup() {
-  pinMode(upPin, INPUT_PULLUP);
-  pinMode(downPin, INPUT_PULLUP);
   // set the digital pin as output:
   pinMode(ledPin, OUTPUT);
-  pinMode(upLedPin, OUTPUT);
-  pinMode(downLedPin, OUTPUT);
-//  lastButtonState[0] = HIGH;
-//  lastButtonState[1] = HIGH;
-//  lastDebounceTime[0] = 0;
-//  lastDebounceTime[1] = 0;
+  pinMode(switchPin, OUTPUT);
 
 #ifdef DEBUG
    Serial.begin(57600);
+   Serial.println("Auto MuisKlikker Analog");
 #endif
 }
 
@@ -78,7 +48,6 @@ void loop()
   static unsigned long lastCheck = 0;
 
   if (currentMillis - lastCheck > buttonCheckInterval) {
-//    handleButtons();
     handleAnalog();
     lastCheck = currentMillis;  
   }
@@ -86,12 +55,12 @@ void loop()
   // different timing for on and off
   // if the LED is off turn it on and vice-versa:
   if (ledState == LOW) {
-    if (currentMillis - previousMillis >= clickDuration) {
+    if (currentMillis - previousMillis >= interval) {
        ledState = HIGH;
        previousMillis = currentMillis;   
     }
   } else {
-    if (currentMillis - previousMillis >= interval) {
+    if (currentMillis - previousMillis >= clickDuration) {
        ledState = LOW;
        // save the last time you blinked the LED 
        previousMillis = currentMillis;   
@@ -99,101 +68,23 @@ void loop()
   }
   // set the LED with the ledState of the variable:
   digitalWrite(ledPin, ledState);
+  digitalWrite(switchPin, ledState);
 }
 
 void handleAnalog() {
   //(-160*1024 + 512*360) / 512
   // doing some fixed num calculation
   const long a = -160;
-  const long c = 360 * 512;
+  const long c = 360l * 512l;
   const long factor = 512;
   
-  int analogValue = analogRead(analogPin);  // 0 - 1023
-  interval = (a * analogValue + c) / factor;
-}
-
-void handleButtons() {
-  static const unsigned long defaultCrement = 50;    // the decrement or increment in ms per keypress
-  static unsigned long crement = 50;    // the decrement of increment per keypress
-
-  int reading = handleDebounce(upPin);
- 
-  switch (reading) {
-    case LOW:
- #ifdef DEBUG
-   Serial.print("L ");
- #endif
-     interval += crement;
-     crement += defaultCrement;     // accellerate moderately
-     digitalWrite(upLedPin, HIGH);
-    break;
-    case HIGH:
-    crement = defaultCrement;
+  long analogValue = analogRead(analogPin);  // 0 - 1023
+  long v = (a * analogValue);
+  interval = (v + c) / factor;
 #ifdef DEBUG
-    Serial.print("H ");
+    Serial.print("ana: ");
+    Serial.print(analogValue);
+    Serial.print(" | int: ");
+    Serial.println(interval);
 #endif
-      digitalWrite(upLedPin, LOW);
-    break;
-  }
-  reading = handleDebounce(downPin);
-  switch (reading) {
-    case LOW:
-      interval = (interval < 100) ? 100 : interval - crement;
-      digitalWrite(downLedPin, HIGH);
-     crement += defaultCrement;     // accellerate moderately
-    break;
-    case HIGH:
-     crement = defaultCrement;
-     digitalWrite(downLedPin, LOW);
-    break;
-  }
-
-}
-
-/* 
- *  returns : -1 : undefined (debouncing)
-              0 : low
-              1 : high
-*/              
-int handleDebounce(int pin) {
-  const unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
-  static int buttonState[BUTTONCOUNT] = {};             // the current reading from the input pin
-  static int lastButtonState[BUTTONCOUNT] = {};   // the previous reading from the input pin
-  static unsigned long lastDebounceTime[BUTTONCOUNT] = {};  // the last time the output pin was toggled
-
-    // read the state of the switch into a local variable:
-  int reading = digitalRead(pin);
-#ifdef DEBUG
-     Serial.print(pin);
-     Serial.print(reading);
-     Serial.print(" ");
-#endif
-
-  int pinIndex = pin - upPin;
-  // check to see if you just pressed the button
-  // (i.e. the input went from LOW to HIGH),  and you've waited
-  // long enough since the last press to ignore any noise:
-
-  // If the switch changed, due to noise or pressing:
-  if (reading != lastButtonState[pinIndex]) {
-    // reset the debouncing timer
-    lastDebounceTime[pinIndex] = millis();
-  }
-
-  lastButtonState[pinIndex] = reading;
-  
-  if ((millis() - lastDebounceTime[pinIndex]) > debounceDelay) {
-    // whatever the reading is at, it's been there for longer
-    // than the debounce delay, so take it as the actual current state:
-
-    // Toggle: if the button state has changed:
-//    if (reading != buttonState[pinIndex]) {
-//      buttonState[pinIndex] = reading;
-//      return reading;
-//    }
-    // just return the buttonState:
-    buttonState[pinIndex] = reading;
-    return reading;
-  }
-  return -1;
 }
---
+++ AutomuisKlikkerAnalogBlynk/AutomuisKlikkerAnalogBlynk.ino
@@ -0,0 +1,185 @@
+/* Blink without Delay
+
+ Turns on and off a light emitting diode (LED) connected to a digital
+ pin, without using the delay() function.  This means that other code
+ can run at the same time without being interrupted by the LED code.
+
+  Version meant for the digispark
+  Version meant for the nano
+  Version meant for a NodeMCU (ESP8266), with the Blynk library for remote toggling
+  
+ Axel Roest
+ This version uses an analog value, derived from a potmeter, to set the click speed
+ 
+ */
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include "secrets.h"
+
+// constants
+
+// #define DEBUG 0
+#ifdef DEBUG
+#define BLYNK_PRINT Serial
+#endif
+
+const int ledPin =  D3;      // the number of the LED pin
+const int switchPin =  D2;      // the number of the Switch pin
+const int analogPin = A0;
+const int onlineSwitch = D4;    // if low, no connection with Blynk
+const int onlineLED = D0;       // show/hide connection with Blynk, on-board LED
+
+
+// Variables will change :
+int ledState = LOW;             // ledState used to set the LED
+long interval = 200;            // interval at which to blink (milliseconds)
+boolean clickEnabled = true;            // remote enabler/disabler
+boolean blynkEnabled = true;            // Blynk enabler/disabler
+
+// Generally, you should use "unsigned long" for variables that hold time
+// The value will quickly become too large for an int to store
+const unsigned long int clickDuration = 40;
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = BLYNK_TOKEN;
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = WIFI_SSID;
+char pass[] = WIFI_PASSWORD;
+
+
+void setup() 
+{
+  // set the digital pin as output:
+  pinMode(ledPin, OUTPUT);
+  pinMode(switchPin, OUTPUT);
+  pinMode(onlineSwitch, INPUT_PULLUP);
+   
+#ifdef DEBUG
+   Serial.begin(57600);
+   Serial.println("Auto MuisKlikker Analog");
+#endif
+
+  Blynk.begin(auth, ssid, pass);
+}
+
+void loop()
+{
+  unsigned long currentMillis = millis();
+  static unsigned long lastBlynkButtonCheck = 0;
+
+  // if click is disabled, we still need to 'end our click's high state'
+  if (clickEnabled || ledState) {
+    handleClick(currentMillis);
+  }
+
+  // check Blynk button every second
+  if (currentMillis - lastBlynkButtonCheck > 1000) {
+    blynkEnabled = boolean(digitalRead(onlineSwitch));
+    digitalWrite(onlineLED, blynkEnabled);
+  }
+  if (blynkEnabled) {
+    Blynk.run();
+  }
+}
+
+///////////////////////////////////////////////////////////
+// Functions
+///////////////////////////////////////////////////////////
+void handleClick(unsigned long currentMillis)
+{
+  static const unsigned long buttonCheckInterval = 50;    // the amount of milliseconds after which we process keys, the key-repeat rate in ms
+  static unsigned long lastCheck = 0;
+  
+  if (currentMillis - lastCheck > buttonCheckInterval) {
+    handleAnalog();
+    lastCheck = currentMillis;  
+  }
+  doPulses(currentMillis);
+}
+
+void handleAnalog() 
+{
+  //(-160*1024 + 512*360) / 512
+  // doing some fixed num calculation
+  const long a = -160;
+  const long c = 360l * 512l;
+  //const long factor = 512;  // 40 ms interval
+  const long factor = 688;  // 30 ms interval
+  
+  long analogValue = analogRead(analogPin);  // 0 - 1023
+  long v = (a * analogValue);
+  long oldInterval = interval;
+  interval = (v + c) / factor;
+
+  if (oldInterval != interval) {
+    // send the new value to Blynk
+    updateInterval(interval);
+  }
+  
+#ifdef DEBUG
+    Serial.print("ana:");
+    Serial.print(analogValue);
+    Serial.print(" | int:");
+    Serial.print(interval);
+
+    if (clickEnabled) {
+      Serial.print(" | Clk");
+    } else {
+      Serial.print(" | ___");
+    }
+
+    if (blynkEnabled) {
+      Serial.println(" | Blynk On");
+    } else {
+      Serial.println(" | Blynk Off");
+    }
+#endif
+}
+
+void doPulses(unsigned long currentMillis) 
+{
+  static unsigned long previousMillis = 0;        // will store last time LED was updated
+
+    // different timing for on and off
+  // if the LED is off turn it on and vice-versa:
+  if (ledState == LOW) {
+    if (currentMillis - previousMillis >= interval) {
+       ledState = HIGH;
+       previousMillis = currentMillis;   
+    }
+  } else {
+    if (currentMillis - previousMillis >= clickDuration) {
+       ledState = LOW;
+       // save the last time you blinked the LED 
+       previousMillis = currentMillis;   
+    }
+  }
+  // set the LED with the ledState of the variable:
+  digitalWrite(ledPin, ledState);
+  digitalWrite(switchPin, !ledState);
+}
+
+// Update virtual pins
+void updateInterval(long interval) {
+  Blynk.virtualWrite(V1, interval);
+}
+
+/// BLYNK STUFF
+BLYNK_WRITE(V0)
+{
+  clickEnabled = boolean(param.asInt());
+}
+
+BLYNK_READ(V0)
+{
+  Blynk.virtualWrite(V0, int(clickEnabled));
+}
+
+BLYNK_READ(V1)
+{
+  updateInterval(interval);
+}
